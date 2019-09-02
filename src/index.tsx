b5762f2b68e8bde5b76c7bcd1377e2e6e05c312d
Shoichi Kaji <skaji@cpan.org>
2019-09-02 12:36:08 +0900

    update

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,10 @@ import * as React from 'react'
 //import { render } from 'react-dom'
 import ReactDOM from 'react-dom';
 
-import { rows } from './data'
-import Thead from './thead'
-import Tbody from './tbody'
-
 
 const Component: React.FC = () => (
     <div>
-        <h1>健康に関する調査</h1>
-        <table>
-            <Thead />
-            <Tbody rows={rows}/>
-        </table>
+    hello
     </div>
 )
 
